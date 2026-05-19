e44894e8ce916a3885efd1641b6d1d571d2b65b3
Audrey <audreythando@gmail.com>
2026-05-19 13:18:31 +0200

    feat: initialize modern frontend redesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,15 @@
-import React, {useState} from 'react';
-import './App.css';
-import Register from './components/Register';
-import { ThemeProvider, createTheme, CssBaseline, Container, Grid } from '@mui/material';
-import {
-  Route,
-  Routes,
-} from "react-router-dom";
-import CreditCard from './components/CreditCard';
-import Navbar from "./components/Navbar/Navbar"
-import CreditCardTable from './components/CreditCardTable';
-import Home from './components/Home';
-// import {CreditCardDetails} from './types/cardDetailsTypes'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DashboardPage from "./pages/DashboardPage";
+import LoginPage from "./pages/LoginPage";
 
-const customTheme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#1a1625', 
-    },
-    secondary: {
-      main: '#ff4136', 
-    },
-  },
-  typography: {
-    fontFamily: 'Arial, sans-serif',
-  },
-});
-
-
-
-interface CardDetails {
-  number: string;
-  name: string;
-  expiry: string;
-  cvc: string;
-  country: string;
-}
-
-
-function App(CardDetails: any) {
-  const [capturedCards, setCapturedCards] = useState<CardDetails[]>([]);
-
-
-  const handleCardSubmit = (card: CardDetails) => {
-    setCapturedCards([...capturedCards, card]);
-  };
-  
-
+function App() {
   return (
-        <ThemeProvider theme={customTheme}>
-        <CssBaseline />
-        <>
-        <Navbar />
-        <Container maxWidth="sm">
-            <Grid
-              container
-              spacing={1}
-              justifyContent="center"
-              alignItems="center"
-              style={{ minHeight: '100vh' }}
-            >
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/register" element={<Register />} />
-                * <Route
-                  path="/credit-card"
-                  element={<CreditCard onCardSubmit={handleCardSubmit} />}
-                />
-
-
-                <Route  path='/table' element={<CreditCardTable creditCards={capturedCards} />} /> 
-              </Routes>
-            </Grid>
-          </Container>
-        </>
-  </ThemeProvider>
-      
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
